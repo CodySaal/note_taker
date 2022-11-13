42ff107db801f0d87139e54b4788f5bd43ea6779
Cody <110630101+CodySaal@users.noreply.github.com>
2022-11-13 10:26:35 -0600

    Added view routes to index.html and notes.html, allowed access to public folder

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+const path = require("path")
 const express = require("express")
 const app = express()
 const PORT = 3000
+
+app.use(express.static("public"))
+
+
+
+
+
+
+
+
+
+
+
+// view (html) routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"))
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
