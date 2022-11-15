191abf944e29ccdbed3958cd3ee383b1037e7ce3
Cody <110630101+CodySaal@users.noreply.github.com>
2022-11-14 22:26:38 -0600

    getNotes request works

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+const fs = require("fs")
 const path = require("path")
 const express = require("express")
 const app = express()
 const PORT = 3000
 
+
 app.use(express.static("public"))
+app.use(express.json())
 
-
+// API routes
+app.get("/api/notes", (req,res) => {
+    // Read db.json content
+    fs.readFile(path.join(__dirname, "db", "db.json"), "utf-8", function(err, data) {
+        if (err) {
+            res.status(500).json(err)
+            return
+        }
+        const json = JSON.parse(data)
+        // respond with the parsed array
+        res.json(json)
+    })
+})
 
 
 
