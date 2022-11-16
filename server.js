1ebfca64e935955c75d6bafa34731f4aa3e54775
Cody <110630101+CodySaal@users.noreply.github.com>
2022-11-16 09:57:30 -0600

    Updated views.js

--- server.js
+++ server.js
@@ -81,13 +81,13 @@ app.delete("/api/notes/:id", (req, res) => {
 
 
 // view (html) routes
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"))
+// })
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "notes.html"))
-})
+// app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "notes.html"))
+// })
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
